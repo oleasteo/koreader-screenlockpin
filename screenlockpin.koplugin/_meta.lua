102b9bf130ae7942ae475b1fea2cecbd843a4a21
Ole Asteo <oleasteo@mailup.net>
2025-10-15 23:07:51 +0200

    chore: add meta information

--- screenlockpin.koplugin/_meta.lua
+++ screenlockpin.koplugin/_meta.lua
@@ -1,8 +1,12 @@
 local _ = require("gettext")
 
 return {
+    -- KOReader meta information
     name = "screenlockpin",
-    version = "2025.10-0",
     fullname = _("ScreenLock PIN"),
     description = _([[Protect your device with a simple PIN.]]),
+
+    -- additional meta information; no effect in code
+    version = "2025.10",
+    repository_url = "https://github.com/oleasteo/koreader-screenlockpin",
 }
