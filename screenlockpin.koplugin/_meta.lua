0c06520a42ecb8a86ea2673436c63b7e457a6ae8
Ole Asteo <oleasteo@mailup.net>
2025-10-15 15:23:24 +0200

    chore: update meta

--- screenlockpin.koplugin/_meta.lua
+++ screenlockpin.koplugin/_meta.lua
@@ -2,6 +2,7 @@ local _ = require("gettext")
 
 return {
     name = "screenlockpin",
+    version = "2025.10-0",
     fullname = _("ScreenLock PIN"),
-    description = _([[Allows you to lock your device with a simple PIN.]]),
+    description = _([[Protect your device with a simple PIN.]]),
 }
