9e9a3568bff4dbcfd6ff372d3682db27a3f45a49
Ole Asteo <oleasteo@mailup.net>
2025-10-15 23:08:42 +0200

    chore: add bundle script

---
+++ bundle.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+root_dir="$(realpath "$(dirname "$0")")"
+target_dir="$root_dir/target"
+name="screenlockpin.koplugin"
+version="2025.10"
+id="$name--$version"
+
+[ -d "$target_dir/bundle" ] && rm -rf "$target_dir/bundle"
+rm -f "$target_dir/release/$id"* 2>/dev/null || true
+
+mkdir -p "$target_dir/release" "$target_dir/bundle"
+cp -r "$name" "$target_dir/bundle/"
+
+cp LICENSE README.md CHANGELOG.md "$target_dir/bundle/$name/"
+
+(
+  cd "$target_dir/bundle"
+  tar -czf ../release/"$id.tar.gz" *
+  zip -qr ../release/"$id.zip" *
+)
