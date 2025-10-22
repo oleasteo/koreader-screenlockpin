e061bbe414e2efbfc967bb2aca22c615df665e36
Ole Asteo <oleasteo@mailup.net>
2025-10-22 18:53:52 +0200

    feat(dialog): use lean dialog implementation
    Most importantly, we don't need a full reinit() call to update the title, as we know it's just a single line => no layout shift.
    As an aside, I don't want dialogs to be movable for settings dialogs.

--- screenlockpin.koplugin/ui/screenlockdialog.lua
+++ screenlockpin.koplugin/ui/screenlockdialog.lua
@@ -1,31 +1,46 @@
-local logger = require("logger")
 local _ = require("gettext")
 local Font = require("ui/font")
-local ButtonDialog = require("ui/widget/buttondialog")
+local FocusManager = require("ui/widget/focusmanager")
 local UIManager = require("ui/uimanager")
+local Blitbuffer = require("ffi/blitbuffer")
+local ButtonTable = require("ui/widget/buttontable")
+local CenterContainer = require("ui/widget/container/centercontainer")
+local Device = require("device")
+local FrameContainer = require("ui/widget/container/framecontainer")
+local Geom = require("ui/geometry")
+local GestureRange = require("ui/gesturerange")
+local LineWidget = require("ui/widget/linewidget")
+local Size = require("ui/size")
+local TextWidget = require("ui/widget/textwidget")
+local VerticalGroup = require("ui/widget/verticalgroup")
+local Screen = Device.screen
+local util = require("util")
 local PinInputState = require("pininputstate")
 
-local ScreenLockDialog = ButtonDialog:extend {
+local ScreenLockDialog = FocusManager:extend {
+    placeholder = "",
+    size_factor = 1.25,
+
+    title = "",
     title_align = "center",
     title_face = Font:getFace("smalltfont"),
+
+    width_factor = 0.9,
+    title_padding = Size.padding.large,
+    title_margin = Size.margin.title,
+
+    state = nil,
+    titleWidget = nil,
+    titleGroup = nil,
+    dialogContent = nil,
+    buttontable = nil,
+    key_events = nil,
+    ges_events = nil,
 }
 
 function ScreenLockDialog:init()
-    if not self.buttons == nil then
-        -- init is called through ButtonDialog:reinit
-        ButtonDialog.init(self)
-        return
-    end
     local ready = false
-    local _valid = false
-    local function on_valid_state(valid)
-        if not ready then return end
-        logger.dbg("ON_VALID_STATE:" .. (valid and "true" or "false"))
-        _valid = valid
-        local submit = self:getButtonById("submit")
-        if valid then submit:enable() else submit:disable() end
-    end
-    local state = PinInputState:new {
+    self.state = PinInputState:new {
         placeholder = self.placeholder,
         size_factor = self.size_factor,
         on_submit = self.on_submit,
@@ -33,20 +48,123 @@ function ScreenLockDialog:init()
         on_update = self.on_update,
         on_display_update = function(title)
             if not ready then return end
-            --self:setTitle(title)
-            self.title = title
-            --assertNoCyclesInWidgetTree(self)
-            self:reinit()
-            UIManager:setDirty(self, "fast")
-            -- ButtonDialog:reinit loses the submit button state :/
-            on_valid_state(_valid)
+            self:setTitle(title)
         end,
-        on_valid_state = on_valid_state,
+        on_valid_state = function(valid)
+            if not ready then return end
+            local submit = self:getButtonById("submit")
+            if valid then submit:enable() else submit:disable() end
+        end,
     }
-    self.buttons = state:makeButtons()
-    self.title = state.display
-    ButtonDialog.init(self)
+
+    local width = math.floor(math.min(Screen:getWidth(), Screen:getHeight()) * self.width_factor)
+
+    if Device:hasKeys() then
+        local back_group = util.tableDeepCopy(Device.input.group.Back)
+        if Device:hasFewKeys() then
+            table.insert(back_group, "Left")
+            self.key_events.Close = { { back_group } }
+        else
+            table.insert(back_group, "Menu")
+            self.key_events.Close = { { back_group } }
+        end
+    end
+
+    if Device:isTouchDevice() then
+        self.ges_events.TapClose = {
+            GestureRange:new { ges = "tap", range = Screen:getSize() }
+        }
+    end
+
+    self.buttontable = ButtonTable:new {
+        buttons = self.state:makeButtons(),
+        width = width - 2 * Size.border.window - 2 * Size.padding.button,
+        show_parent = self,
+    }
+    local buttontable_width = self.buttontable:getSize().w
+
+    local title_padding = Size.padding.default
+    local title_margin = Size.margin.default
+
+    self.layout = self.buttontable.layout
+    self.buttontable.layout = nil
+
+    self.titleWidget = TextWidget:new {
+        text = self.state.display,
+        width = buttontable_width - 2 * (title_padding + title_margin),
+        face = self.title_face,
+        bold = true,
+    }
+    self.titleGroup = VerticalGroup:new {
+        align = self.title_align,
+        self.titleWidget,
+    }
+    self.dialogContent = VerticalGroup:new {
+        FrameContainer:new {
+            padding = title_padding,
+            padding_top = title_padding * 2,
+            padding_bottom = title_padding * 2,
+            margin = title_margin,
+            bordersize = 0,
+            self.titleGroup
+        },
+        LineWidget:new {
+            background = Blitbuffer.COLOR_GRAY,
+            dimen = Geom:new { w = buttontable_width, h = Size.line.medium },
+        },
+        self.buttontable,
+    }
+    self[1] = CenterContainer:new {
+        show_parent = self,
+        dimen = Screen:getSize(),
+        FrameContainer:new {
+            background = Blitbuffer.COLOR_WHITE,
+            bordersize = Size.border.window,
+            radius = Size.radius.window,
+            padding = Size.padding.button,
+            padding_top = 0,
+            padding_bottom = 0,
+            self.dialogContent,
+        },
+    }
+
     ready = true
+end
+
+function ScreenLockDialog:getButtonById(id)
+    return self.buttontable:getButtonById(id)
+end
+
+function ScreenLockDialog:setTitle(title)
+    self.titleWidget:setText(title)
+    self.titleWidget:updateSize()
+    self.titleGroup:resetLayout()
+    self.dialogContent:resetLayout()
+    UIManager:setDirty(self, "fast")
+end
+
+function ScreenLockDialog:onShow()
+    UIManager:setDirty(self, function()
+        return "ui", self[1][1].dimen
+    end)
+end
+
+function ScreenLockDialog:onCloseWidget()
+    UIManager:setDirty(nil, function()
+        return "flashui", self[1][1].dimen
+    end)
+end
+
+function ScreenLockDialog:onClose()
+    UIManager:close(self)
+    return true
+end
+
+function ScreenLockDialog:onTapClose(_, ges)
+    if ges.pos:notIntersectWith(self[1][1].dimen) then
+        self:onClose()
+    end
+    return true
 end
 
 return ScreenLockDialog
