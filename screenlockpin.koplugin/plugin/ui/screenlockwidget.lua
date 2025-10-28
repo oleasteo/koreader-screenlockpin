94687b6eb2b30742ce68c02b851201ce7e7a3c04
Ole Asteo <oleasteo@mailup.net>
2025-10-28 19:56:53 +0100

    perf: remove obsolete calculations

--- screenlockpin.koplugin/plugin/ui/screenlockwidget.lua
+++ screenlockpin.koplugin/plugin/ui/screenlockwidget.lua
@@ -12,7 +12,6 @@ local ScreenLockWidget = VerticalGroup:extend {
     ui_root = nil,
     state = nil,
     width_factor = 0.9,
-    margin = 0,
     title_face = Font:getFace("smalltfont"),
 }
 
@@ -24,13 +23,13 @@ function ScreenLockWidget:init()
         on_display_update = function(text) if self[1] then self[1]:setText(text) end end,
         on_update = self.on_update,
     }
-    local width = math.floor(Screen:getWidth() * self.width_factor) - 2 * self.margin
+    local width = math.floor(Screen:getWidth() * self.width_factor)
     self[1] = TextBoxLiteWidget:new {
         ui_root = self.ui_root,
         text = self.state.display,
         face = self.title_face,
         width = width,
-        padding = math.max(Size.item.height_default, self.margin),
+        padding = Size.item.height_default,
     }
     self[2] = ButtonTable:new {
         buttons = self.state:makeButtons(),
@@ -40,7 +39,7 @@ function ScreenLockWidget:init()
 end
 
 function ScreenLockWidget:onScreenResize(screen_dimen)
-    local width = math.floor(screen_dimen.w * self.width_factor) - 2 * self.margin
+    local width = math.floor(screen_dimen.w * self.width_factor)
     local textbox = self[1]
     local buttontable = self[2]
     buttontable.width = width
@@ -50,18 +49,6 @@ function ScreenLockWidget:onScreenResize(screen_dimen)
     textbox:free(true)
     textbox.width = width
     self:resetLayout()
-end
-
-function ScreenLockWidget:paintTo(bb, x, y)
-    VerticalGroup.paintTo(self, bb, x, y + self.margin)
-end
-
-function ScreenLockWidget:getSize()
-    local content_size = VerticalGroup.getSize(self)
-    return {
-        w = content_size.w + 2 * self.margin,
-        h = content_size.h + self.margin,
-    }
 end
 
 return ScreenLockWidget
