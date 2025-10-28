a76da23b32200ab1967771e20a7ba52e186218b1
Ole Asteo <oleasteo@mailup.net>
2025-10-28 19:21:59 +0100

    feat(overlay): slightly mitigate ghosting

--- screenlockpin.koplugin/plugin/ui/lockscreenframe.lua
+++ screenlockpin.koplugin/plugin/ui/lockscreenframe.lua
@@ -39,6 +39,9 @@ function LockScreenFrame:init()
 
     local content_container = FrameContainer:new {
         background = Blitbuffer.COLOR_WHITE,
+        -- half-bright gray border plays nice with most wallpapers and mitigates
+        -- ghosting a little
+        color = Blitbuffer.COLOR_GRAY_7,
         padding = 0,
         self.widget,
     }
@@ -66,11 +69,14 @@ function LockScreenFrame:getRefreshRegion()
         self._refresh_region = self[1].dimen
     else
         local content_size = self[1][1]:getSize()
+        -- grow refresh region a tiny bit; by observation, this seems to improve
+        -- the ghosting a little
+        local outset = Screen:scaleBySize(2)
         self._refresh_region = Geom:new {
-            x = math.floor((Screen:getWidth() - content_size.w)/2),
-            y = math.floor((Screen:getHeight() - content_size.h)/2),
-            w = content_size.w,
-            h = content_size.h,
+            x = math.max(0, math.floor((Screen:getWidth() - content_size.w)/2) - outset),
+            y = math.max(0, math.floor((Screen:getHeight() - content_size.h)/2) - outset),
+            w = math.min(Screen:getWidth(), content_size.w + outset * 2),
+            h = math.min(Screen:getHeight(), content_size.h + outset * 2),
         }
     end
     return self._refresh_region
