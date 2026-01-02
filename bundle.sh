4b1080cffc6e0d1496aa41f27e6649d8cf854095
Ole Asteo <oleasteo@mailup.net>
2026-01-02 02:17:17 +0100

    build: release v2026.01

--- bundle.sh
+++ bundle.sh
@@ -3,7 +3,7 @@
 root_dir="$(realpath "$(dirname "$0")")"
 target_dir="$root_dir/target"
 name="screenlockpin.koplugin"
-version="2025.12-1"
+version="2026.01"
 id="$name--$version"
 
 [ -d "$target_dir/bundle" ] && rm -rf "$target_dir/bundle"
