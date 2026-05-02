aaa4cd25b4b81f932e0b695c5da8cbd172446d74
Ole Asteo <oleasteo@mailup.net>
2026-05-02 17:15:14 +0200

    build(bundle): get version from _meta.lua

--- bundle.sh
+++ bundle.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 root_dir="$(realpath "$(dirname "$0")")"
 target_dir="$root_dir/target"
 name="screenlockpin.koplugin"
-version="2026.02"
+version=$(grep -F 'version =' "$root_dir/$name/_meta.lua" | cut -d '"' -f2)
 id="$name--$version"
+
+echo "Bundling for release: $id"
 
 [ -d "$target_dir/bundle" ] && rm -rf "$target_dir/bundle"
 rm -f "$target_dir/release/$id"* 2>/dev/null || true
@@ -14,8 +18,6 @@ cp -r "$root_dir/$name" "$target_dir/bundle/"
 
 cp "$root_dir/LICENSE" "$root_dir/README.md" "$root_dir/CHANGELOG.md" "$target_dir/bundle/$name/"
 
-(
-  cd "$target_dir/bundle"
-  tar -czf ../release/"$id.tar.gz" *
-  zip -qr ../release/"$id.zip" *
-)
+cd "$target_dir/bundle"
+tar -czf ../release/"$id.tar.gz" *
+zip -qr ../release/"$id.zip" *
